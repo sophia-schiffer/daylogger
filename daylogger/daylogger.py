075c6a473d0eace4e6162d407b7abca772735e74
Sophia Schiffer <mbox.sophia@gmail.com>
2023-12-21 22:48:19 -0800

    added first entry

--- daylogger/daylogger.py
+++ daylogger/daylogger.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''Updates daylogger.txt file based on day and logger times.'''
 
 import datetime
@@ -13,5 +14,30 @@ date = datetime.date.today()
 new_day = False
 if date.year > int(doc_date[2]):
     new_day = True
+elif date.month > int(doc_date[0]):
+    new_day = True
+elif date.day > int(doc_date[1]):
+    new_day = True
+else:
+    new_day = False
 
-print(new_day)
+if new_day:
+    with open("daylogger.txt",'w') as dl:
+        dl.write(str(date.month)+"-"+str(date.day)+"-"+str(date.year)+"\n\n")
+        string = "Today my reason is: "
+        dl.write(string.rstrip("\n"))
+        reason = input("What is your reason today, Sophia?: ")
+        dl.write(reason+"\n\n")
+
+else:
+    first_entry = False
+    with open("daylogger.txt",'r') as dl:
+        lines = dl.readlines()
+        num_lines = len(lines)
+        if num_lines < 4:
+            first_entry = True
+        
+        if not first_entry:
+            prev_goal = lines[-1]
+            print("Your previous goal was:", prev_goal)
+
