4f3e713284048c6d48d3cc65f78ee46b81b5ecd9
Sophia Schiffer <mbox.sophia@gmail.com>
2023-12-24 14:38:43 -0800

    added birthdays

--- daylogger/daylogger.py
+++ daylogger/daylogger.py
@@ -11,15 +11,7 @@ doc_date = first_line.split("-")
 doc_date[2] = doc_date[2][:4]
 
 date = datetime.date.today()
-
-new_day = False
-if date.year > int(doc_date[2]):
-    new_day = True
-    print("\nHappy "+str(date.year)+"!\n\n")
-    
-elif date.month > int(doc_date[0]):
-    new_day = True
-    month_names = {
+month_names = {
     1: "January",
     2: "February",
     3: "March",
@@ -32,14 +24,65 @@ elif date.month > int(doc_date[0]):
     10: "October",
     11: "November",
     12: "December"
-    }
-    month = month_names[date.month]
+}
+month = month_names[date.month]
+
+new_day = False
+if date.year > int(doc_date[2]):
+    new_day = True
+    print("\nHappy "+str(date.year)+"!\n\n")
+
+elif date.month > int(doc_date[0]):
+    new_day = True
     print("\nHappy "+month+"!\n\n")
 
 elif date.day > int(doc_date[1]):
     new_day = True
 else:
     new_day = False
+
+# Permanent Holidays
+celebrate = False
+if month == "February":
+    if date.day == 2:
+        celebrate = True
+        holiday = "Groundhog Day"
+    elif date.day == 14:
+        celebrate = True
+        holiday = "Valentine's Day <3"
+elif month == "March":
+    if date.day == 17:
+        celebrate = True
+        holiday = "St. Patty's Day"
+elif month == "November":
+    if date.day == 11:
+        celebrate = True
+        holiday = "Veteran's Day"
+elif month == "December":
+    if date.day == 2:
+        celebrate = True
+        age = str(date.year - 2001)
+        holiday = age+"nd Birthday"
+    elif date.day == 25:
+        print("\nMerry Christmas!\n\n")
+
+# Annual holidays
+#TODO: Easter, Thanksgiving
+
+if new_day and celebrate:       
+    print("\nHappy "+holiday+"!\n\n")
+
+birthdays = {
+    "Andrea": [4,19],
+    "Markus": [4,28],
+    "Mama & Lauren": [12,14],
+    "Sylvia": [5,1],
+    "Papa": [9,7]
+}
+if new_day:
+    for key in birthdays.keys():
+        if birthdays[key][0] == date.month and birthdays[key][1] == date.day:
+            print("\nIt's "+key+"'s birthday today!")
 
 if new_day:
     with open("daylogger.txt",'w') as dl:
