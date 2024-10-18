c7283b6a2141997c6487132955226666bcfdceb4
sophia-schiffer <mbox.sophia@gmail.com>
2024-10-18 12:13:43 -0500

    began birthday sorting script

---
+++ daylogger/birthdays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Script that informs user of birthdays coming up
+    Uses terminal command to output sorted birthdays
+    
+    -s month, year
+'''
+
+import datetime
+
+if __name__ == '__main__':
+    today = datetime.date.today()
