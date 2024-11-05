e37956b2fdad4320fe982617058caa5b9de769e9
sophia-schiffer <mbox.sophia@gmail.com>
2024-11-04 19:18:47 -0600

    birthday input works

--- daylogger/birthdays.py
+++ daylogger/birthdays.py
@@ -11,11 +11,14 @@ import getopt
 
 def get_args():
     entry = None
+    new_name = ''
+    new_month = ''
+    new_day = ''
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hs:",["sort="])
+        opts, args = getopt.getopt(sys.argv[1:],"hs:n:m:d:",["sort="])
     except getopt.GetoptError:
         print("Error: Invalid arguments")
-        print('Usage: birthdays.py -s <timeline string>')
+        print('Usage: birthdays.py -s <timeline string> -n <name> -m <month> -d <day>')
         return
     for opt, arg in opts:
         if opt == '-h':
@@ -23,8 +26,14 @@ def get_args():
             sys.exit(2)
         elif opt in ("-s", "--sort"):
             entry = arg
+        elif opt in ("-n", "--name"):
+            new_name = arg
+        elif opt in ("-m", "--month"):
+            new_month = arg
+        elif opt in ("-d", "--day"):
+            new_day = arg
     
-    return entry
+    return entry, new_name, new_month, new_day
 
 def read_birthdays():
     with open("birthdays.txt",'r') as bd:
@@ -45,12 +54,28 @@ def print_birthdays(month, birthdays):
     for bday in birthdays:
         print(str(bday[0])+": "+str(month)+"/"+str(bday[1]))
 
+def input_new(name, month, day):
+    if month == '':
+        print("Error: No month entered")
+        return
+    if day == '':
+        print("Error: No day entered")
+        return
+
+    with open("birthdays.txt",'a') as bd:
+        bd.write(name+":"+month+":"+day+"\n")
+
+    print("Birthday added")
+
 if __name__ == '__main__':
     today = datetime.date.today()
-    timeline = get_args()
+    timeline, name, month, day = get_args()
     birthdays = read_birthdays()
     relevant_birthdays = {}
     
+    if name != '':
+        input_new(name, month, day)
+
     if timeline == 'month':
         for bday in birthdays:
             if int(bday[1]) == today.month:
